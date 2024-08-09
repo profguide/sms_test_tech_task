d5d66f3e0feaa45a24f7bf2ec9c5a84a18b0fe48
adavydov <david.tema@yandex.ru>
2024-08-09 12:54:05 +0200

    add main page with test loader

---
+++ src/Controller/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/')]
+final class MainController extends AbstractController
+{
+    #[Route('')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig');
+    }
+}
