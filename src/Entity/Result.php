1a36622895253d305ea52e86ba0ffb479a4b74bb
adavydov <david.tema@yandex.ru>
2024-08-09 12:45:07 +0200

    add Entity Result, migration

--- src/Entity/Result.php
+++ src/Entity/Result.php
@@ -4,12 +4,31 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use App\Repository\ResultRepository;
 use App\Test\AnswersHolder;
 use App\Test\AnswersSerializer;
+use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\Entity(repositoryClass: ResultRepository::class)]
 final class Result
 {
+    #[ORM\Id]
+    #[ORM\Column]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    private ?int $id;
+
+    #[ORM\Column(type: 'text', nullable: false)]
     private string $answers;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
 
     public function getAnswers(): string
     {
---
+++ src/Service/ResultService.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\Result;
+use App\Repository\ResultRepository;
+
+final readonly class ResultService
+{
+    public function __construct(private ResultRepository $results)
+    {
+    }
+
+    public function create(Result $result): void
+    {
+        $this->results->create($result);
+    }
+}
