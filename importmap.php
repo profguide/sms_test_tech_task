08f04e813ecba3706f75fee6a9cf50f968da318c
adavydov <david.tema@yandex.ru>
2024-08-09 13:23:56 +0200

    add bootstrap

--- importmap.php
+++ importmap.php
@@ -22,4 +22,14 @@ return [
     'axios' => [
         'version' => '1.7.3',
     ],
+    'bootstrap' => [
+        'version' => '5.3.3',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.3',
+        'type' => 'css',
+    ],
 ];
